93ac7fe8388c3be4557f3b9d1ea64ef91afae901
ImHarim <168340202+ImHarim@users.noreply.github.com>
2024-11-10 14:46:19 +0900

    Update divisor.py

--- .github/divisor.py
+++ .github/divisor.py
@@ -1,11 +1,10 @@
-import sys
+def find_divisors(number):
+    divisors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            divisors.append(i)
+    return divisors
 
-# 명령줄 인자로 받은 수를 정수로 변환
-number = int(sys.argv[1])
-
-# 1부터 number까지 순회하며 나누어 떨어지는 수를 찾기
-for i in range(1, number + 1):
-    if number % i == 0:
-        print(i, end=" ")
-
-print()
+if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+    print(f"Divisors of {number} are: {find_divisors(number)}")
